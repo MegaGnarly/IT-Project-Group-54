a1fc8c0312cd4d9663c2432a068d3a65ac9785d4
JackYuHE <yhhe4@student.unimelb.edu.au>
2022-08-24 15:18:10 +1000

    adding user model

---
+++ model/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+var userSchema = new mongoose.Schema({
+    first_name: {type: String, required:true},
+    last_name: {type: String, required: true},
+    user_name: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+});
+
+// password comparison function
+userSchema.methods.verifyPassword = function (password, callback) {
+    console.log(this.password)
+    bcrypt.compare(password, this.password, (err, valid) => {
+        if (password === this.password){
+            valid = true;
+        }
+        callback(err, valid)
+    })
+    
+}
+
+const SALT_FACTOR = 10
+
+// hash password before saving
+userSchema.pre('save', function save(next) {
+    const user = this// go to next if password field has not been modified
+    if (!user.isModified('password')) {
+        return next()
+    }
+
+    // auto-generate salt/hash
+    bcrypt.hash(user.password, SALT_FACTOR, (err, hash) => {
+        if (err) {
+            return next(err)
+        }
+        //replace password with hash
+        user.password = hash
+        next()
+    })
+})
+
+const User = mongoose.model('user', userSchema)
+  
+module.exports = User
