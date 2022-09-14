f0c0d59dc002d6837d64db298285a58f6257f657
BDANIELSEN <bdanielsen@student.unimelb.edu.au>
2022-09-14 12:12:27 +1000

    Fixed login page and started on responsive design

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -31,12 +31,13 @@ authRouter.get('/user', isAuthenticated, (req, res) => {
 
 // Login page (with failure message displayed upon login failure)
 authRouter.get('/login', (req, res) => {
-    if (isAuthenticated()){
+    /* if (isAuthenticated()){
         res.redirect('/user')
     }
     else {
         res.render('login.hbs', { flash: req.flash('error'), title: 'Login', layout: 'main' })
-    }
+    } */
+    res.render('login.hbs', { flash: req.flash('error'), title: 'Login', layout: 'main' })
 })
 
 authRouter.post('/login',
