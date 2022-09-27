48eb0cdd3c374b10ff99d5c5b2883f7f69ff1862
JackYuHE <yhhe4@student.unimelb.edu.au>
2022-09-27 13:51:41 +1000

    test

--- __tests__/integrationTest/userStories10.test.js
+++ __tests__/integrationTest/userStories10.test.js
@@ -2,8 +2,9 @@ const request = require('supertest');
 const app = require("../../app");
 
 
-//User stories 1
-//As an Angler I want to be able to accurately document details of my catch in order to share these details with my friends
+//User stories 10
+//As an Angler, I want to make sure others can not login to my account
+//in order to protect my fishing record
 describe("integration test of user stories 10", ()=>{
     test("case1: rendering the details of an existing fish which cannot be updated", ()=>{
         return request(app).get('/fishDetails/631141f6f2b83d25661fe8af').then((response) => {
